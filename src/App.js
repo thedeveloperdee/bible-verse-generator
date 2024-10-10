eeffd4f89e77f2a1e014581e20405571fc927a14
Dionne Pennycooke <deemarie@Dionnes-MacBook-Pro.local>
2024-10-10 15:38:05 +0100

    Removed title and button for verse of the day

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [verse, setVerse] = useState('');
   const [reference, setReference] = useState('');
 
-  const fetchVerse = () => {
-    fetch('https://labs.bible.org/api/?passage=random&type=json&version=nlt')
-      .then(response => response.json())
-      .then(data => {
-        setVerse(`"${data[0].text.trim()}"`); // Using .trim() to remove any extra spaces
-        setReference(`~ ${data[0].bookname} ${data[0].chapter}:${data[0].verse}`);
-      })
-      .catch(err => console.error(err));
-  };
+  useEffect(() => {
+    const storedVerse = JSON.parse(localStorage.getItem('dailyVerse'));
+    const storedDate = localStorage.getItem('verseDate');
+    const today = new Date().toDateString();
+
+    if (storedVerse && storedDate === today) {
+      setVerse(storedVerse.verse);
+      setReference(storedVerse.reference);
+    } else {
+      fetch('https://labs.bible.org/api/?passage=random&type=json&version=nlt')
+        .then(response => response.json())
+        .then(data => {
+          const newVerse = `"${data[0].text.trim()}"`;
+          const newReference = `~ ${data[0].bookname} ${data[0].chapter}:${data[0].verse}`;
+          setVerse(newVerse);
+          setReference(newReference);
+  
+          localStorage.setItem('dailyVerse', JSON.stringify({ verse: newVerse, reference: newReference }));
+          localStorage.setItem('verseDate', new Date().toDateString());
+        })
+        .catch(err => console.error(err));
+    }
+  }, []); 
 
   return (
     <div className="App">
-      <h1>Random Bible Verse Generator</h1>
-      <button onClick={fetchVerse}>Get a Bible Verse</button>
       {verse && (
         <div className="verse-container">
           <p className="verse-text">{verse}</p>
